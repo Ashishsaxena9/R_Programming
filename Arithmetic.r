df9a8a71d978de302bd0b9b7c505417490024cea
Ashish Saxena <40749226+Ashishsaxena9@users.noreply.github.com>
2018-08-16 21:21:08 +0530

    Create Arithmetic.r

---
+++ Arithmetic.r
@@ -0,0 +1,39 @@
+Arithmetic With R
+
+
+# Calculate 3 + 4
+
+3 + 4
+
+
+# Calculate 6 + 1
+6 + 1
+
+
+# An addition
+
+5 + 5 
+
+
+# A subtraction
+
+5 - 5 
+
+
+# A multiplication
+
+3 * 5
+
+
+# A division
+(5 + 5) / 2 
+
+
+# Exponentiation
+
+2^5
+
+
+#Modulo
+
+28%%6
